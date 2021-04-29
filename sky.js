9378262a195405ce5dc2b0de4dfb996a94003031
jacksonkoh8 <74502141+jacksonkoh8@users.noreply.github.com>
2021-04-29 11:12:22 +0800

    Delete sky.js

--- sky.js
+++
@@ -1,8 +0,0 @@
-function skyCTRL() {
-  var sky = document.getElementById("sky");
-  if (sky.getAttribute("opacity")=="0") {
-    sky.setAttribute("opacity","0.95");
-   } else {
-    sky.setAttribute("opacity","0");
-  }
-}
